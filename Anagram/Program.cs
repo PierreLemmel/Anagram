c9fbd71df8921fe4e770772af41973c73eb3f178
Pierre LEMMEL <pierre.lemmel@ocea-smart-building.fr>
2018-12-23 22:01:33 +0100

    First project

---
+++ Anagram/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Anagram
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Début du programme");
+            Console.WriteLine("Chargement de la liste des mots existants");
+
+            ILookup<int, char[]> wordsLookup = GetWordsLookup();
+
+            while (true)
+            {
+                Console.WriteLine("Saisissez un ou plusieurs mots et appuyez sur entrée");
+
+                string input = Console.ReadLine();
+                string cleanInput = input.RemovePunctuation().ToLower();
+
+                IReadOnlyDictionary<char, LetterOccurences> data = GetInputData(cleanInput);
+
+                IEnumerable<char[]> scannedWords = wordsLookup[cleanInput.Length];
+
+                ICollection<string> results = new List<string>();
+                foreach (char[] chars in scannedWords)
+                {
+                    if (ScanWord(data, chars))
+                        results.Add(new string(chars));
+
+                    foreach (var kvp in data)
+                        kvp.Value.Reset();
+                }
+
+                Console.WriteLine();
+
+                if (results.Any())
+                {
+                    Console.WriteLine("Liste des anagrammes:");
+                    Console.WriteLine();
+
+                    foreach (string result in results)
+                        Console.WriteLine($"    {result}");
+
+                }
+                else
+                    Console.WriteLine("Oups, aucun résultat trouvé :'(");
+
+                Console.WriteLine(); 
+            }
+        }
+
+        private static bool ScanWord(IReadOnlyDictionary<char, LetterOccurences> data, char[] chars)
+        {
+            foreach (char c in chars)
+            {
+                if (data.TryGetValue(c, out LetterOccurences occurence) && occurence.RemoveIfPossible())
+                    continue;
+                else
+                    return false;
+            }
+
+            return true;
+        }        
+
+        private static ILookup<int, char[]> GetWordsLookup()
+        {
+            string[] allWords = File.ReadAllLines("words.txt");
+            ILookup<int, char[]> wordsLookup = allWords
+                .Select(str => str.RemoveDiacritics().ToLower())
+                .ToLookup(
+                    str => str.Length,
+                    str => str.ToCharArray()
+                );
+
+            return wordsLookup;
+        }
+
+        private static string RemoveDiacritics(this string input)
+        {
+            if (input is null) throw new ArgumentNullException(nameof(input));
+
+            string normalizedString = input.Normalize(NormalizationForm.FormD);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in normalizedString)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                    sb.Append(c);
+            }
+
+            string result = sb.ToString().Normalize(NormalizationForm.FormC);
+            return result;
+        }
+
+        private static string RemovePunctuation(this string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach(char c in input)
+            {
+                if (char.IsLetter(c))
+                    sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+
+        private static IReadOnlyDictionary<char, LetterOccurences> GetInputData(string input)
+        {
+            IReadOnlyDictionary<char, LetterOccurences> wordData = input
+                .ToCharArray()
+                .GroupBy(c => c)
+                .ToDictionary(g => g.Key, g => new LetterOccurences(g.Count()));
+            return wordData;
+        }
+
+        private class LetterOccurences
+        {
+            public LetterOccurences(int total)
+            {
+                this.total = total;
+                current = total;
+            }
+
+            public bool RemoveIfPossible()
+            {
+                if (current > 0)
+                {
+                    current--;
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+            public void Reset() => current = total;
+
+            private readonly int total;
+            private int current;
+        }
+    }
+}
---
+++ ListeDeMotDumper/Program.cs
@@ -0,0 +1,68 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListeDeMotDumper
+{
+    public static class Program
+    {
+        private const string baseUrl = "https://www.listesdemots.net/";
+        private const int maxPage = 898;
+
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Début de l'aspiration du site www.listesdemots.net");
+
+            Uri baseUri = new Uri(baseUrl);
+            HttpClient httpClient = new HttpClient();
+
+            List<string> allWords = new List<string>();
+
+            for (int page = 1; page <= maxPage; page++)
+            {
+                Console.WriteLine($"Aspiration de la page {page}");
+
+                Uri url = new Uri(baseUri, GetRelativeUrl(page));
+                HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Get, url);
+
+                HttpResponseMessage response = await httpClient.SendAsync(httpRequest);
+
+                using (Stream stream = await response.Content.ReadAsStreamAsync())
+                using (Stream decompressed = new GZipStream(stream, CompressionMode.Decompress))
+                using (StreamReader reader = new StreamReader(decompressed))
+                {
+                    string htmlContent = reader.ReadToEnd();
+
+                    HtmlDocument doc = new HtmlDocument();
+                    doc.LoadHtml(htmlContent);
+
+                    HtmlNode node = doc.DocumentNode.SelectSingleNode("//span[@class='mot']").FirstChild;
+                    string[] words = node.InnerText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    allWords.AddRange(words);
+                }
+
+                Console.WriteLine($"Page {page} aspirée");
+            }
+
+            File.WriteAllLines("result.txt", allWords);
+
+            httpClient.Dispose();
+        }
+
+        private static string GetRelativeUrl(int page)
+        {
+            if (page == 1)
+                return "touslesmots.htm";
+            else
+                return $"touslesmotspage{page}.htm";
+        }
+    }
+}
